b78d3d603b626be409e833152371dc30ff128948
guillaume <guillaume@takewalks.com>
2019-06-18 14:30:19 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-drawer-layout
+    
+      >
+    
+      <div class="drawer" slot="drawer">
+        <div class="text">This is drawer</div>
+        <div v-for="(val, index) in 45" :key="index">
+          <div class="menu-item">
+            menu #{{index}}
+          </div>
+        </div>
+      </div>
+      <div class="content" slot="content">
+        <div class="text">This is content.</div>
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+      </div>
+    </vue-drawer-layout>
   </div>
 </template>
 
@@ -24,5 +40,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.menu-item{
+  padding: 15px;
+}
+.drawer{
+  background-color: antiquewhite;
+  overflow-y: auto;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DrawerLayout from 'vue-drawer-layout'
 
+Vue.use(DrawerLayout)
 Vue.config.productionTip = false
 
 new Vue({
